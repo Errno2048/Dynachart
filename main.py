7e5d306797a0b2852d3399f983b7eb0f8d83bf9c
Errno1024 <2541746271@qq.com>
2021-08-01 10:44:46 +0800

    debug

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser.add_argument('--type', '-t', choices=('dynamite', 'dynamix'), type=str, default=None)
     parser.add_argument('--scale', '-s', type=float, default=0.4)
     parser.add_argument('--speed', '-S', type=float, default=0.8)
-    parser.add_argument('--page_limit', '-l', type=int, default=32)
+    parser.add_argument('--page_limit', '-l', type=int, default=16)
     parser.add_argument('--bar_span', '-b', type=int, default=2)
     parser.add_argument('--encoding', '-E', type=str, default='utf8')
     parser.add_argument('--verbose', '-v', action='store_true', default=False)
@@ -38,11 +38,11 @@ if __name__ == '__main__':
                 ftarget = fname[:pt] + '.png'
             else:
                 ftarget = fname + '.png'
-            with open(fname, 'r', encoding=args.encoding) as F:
+            with open(f, 'r', encoding=args.encoding) as F:
                 chart = reader(F.read())
             board = Board(scale=args.scale, time_limit=args.page_limit, speed=args.speed, bar_span=args.bar_span)
             img = board.generate(chart)
-            img.save(ftarget)
+            img.save(os.path.join(os.path.dirname(f), ftarget))
             if args.verbose:
                 print(f'Saved the overview to "{ftarget}".', file=sys.stderr)
         except Exception:
